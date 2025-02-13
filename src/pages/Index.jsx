125c05e5f0adf04332c6ad664548a2e23d2387a7
Edison Matos <edison.matos@live.com>
2025-02-13 17:34:27 -0300

    feat: Atualiza webp

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -38,7 +38,7 @@ export default function Index() {
       <Faq />
       <FooterSocial
         addres={false}
-        instagram={true}
+        instagram={false}
         facebook={false}
         linkedin={false}
         x={false}
